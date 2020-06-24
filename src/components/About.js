25633ff0a55625b6044bd11683779a5748fe2d87
Henry Moss <htmoss7@gmail.com>
2020-06-24 15:04:54 -0700

    Updated about page

--- src/components/About.js
+++ src/components/About.js
@@ -5,7 +5,32 @@ const About = () => {
 		<div className='about'>
 			<h1 className='page-title'>About</h1>
 			<p>Created by Henry Moss</p>
-			<p>Version 1.1.0</p>
+			<p>Version 1.2.0</p>
+			<div className='about-instructions'>
+				<h2>How does this work ?</h2>
+				<h3>1. List Movies</h3>
+				<p>
+					In the List tab of the navbar, search for movies and tv shows that you
+					want to make a note of seeing one day. add them to your list by
+					hovering over the movie and pressing the "Add" button. You can also
+					find more info on the movie by clicking the plus button on the top
+					right of the poster.
+				</p>
+				<h3>2. Rate your movies</h3>
+				<p>
+					Once you watch one of the movies on your list, rate the movie by
+					clicking the checkbox to the right of the movie title. On the popup,
+					rate your movie on a score from one to ten. You can now find your
+					movie on the Ratings tab of the website. Feel free to view your
+					ratings in either the list view or poster view by clicking on their
+					respective buttons.
+				</p>
+				<h3>That's all, folks!</h3>
+				<p>
+					Currently, that's the full funtionality of the website. I'll be trying
+					my best to add new features when I can!
+				</p>
+			</div>
 		</div>
 	);
 };
--- src/components/Ratings/RatingListToggle.js
+++ src/components/Ratings/RatingListToggle.js
@@ -4,22 +4,22 @@ import { PosterToggleContext } from '../../context/PosterToggleContext';
 let theme = 'left';
 
 const RatingListToggle = () => {
-	const { showPosters, setShowPosters } = useContext(PosterToggleContext);
+	const { setShowPosters } = useContext(PosterToggleContext);
 
-	const onClick = (e) => {
-		setShowPosters(!showPosters);
-		if (showPosters) {
-			theme = 'left';
-		} else {
-			theme = 'right';
-		}
+	const onListClick = (e) => {
+		setShowPosters(false);
+		theme = 'left';
+	};
+	const onPosterClick = (e) => {
+		setShowPosters(true);
+		theme = 'right';
 	};
 	return (
 		<div className='view-btns'>
-			<button className={`btn btn-list-${theme}`} onClick={onClick}>
+			<button className={`btn btn-list-${theme}`} onClick={onListClick}>
 				List View
 			</button>
-			<button className={`btn btn-poster-${theme}`} onClick={onClick}>
+			<button className={`btn btn-poster-${theme}`} onClick={onPosterClick}>
 				Poster View
 			</button>
 		</div>
